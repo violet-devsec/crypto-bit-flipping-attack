c15356635a96df5a6c2d70c1906478d7c15b90ea
violet-devsec <titto.tvm@hotmail.com>
2023-02-26 05:24:13 +0530

    added server code

--- server/vuln_server.py
+++ server/vuln_server.py
@@ -20,6 +20,26 @@ def main(s):
     send_msg(s, user +"'s password: ")
     passwd = s.recv(4096).decode().strip()
 
+    msg = 'logged_username=' + user + '&password=' + passwd
+
+    try:
+        assert('admin&password=goBigDawgs123' not in msg)
+    except AssertionError:
+        send_msg(s, 'You cannot login as an admin from an external IP. Good bye!')
+        raise
+
+    send_msg(s, "Leaked ciphertext: " + encrypt_data(msg)+'\n')
+    send_msg(s, "Enter ciphertext: ")
+
+    enc_msg = s.recv(4096).decode().strip()
+    check = False
+
+    try:
+        check = decrypt_data(enc_msg)
+    except Exception as e:
+        send_msg(s, str(e) + '\n')
+        s.close()
+
 if __name__ == '__main__':
     socketserver.ThreadingTCPServer.allow_resuse_address = True
     server = socketserver.ThreadingTCPServer(('0.0.0.0', 3000), TaskHandler)
